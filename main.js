09fe4a0e15ca1122d1c79353b9590b284a737663
boomafromboomiland <lubomira.kovacova@avon.com>
2022-03-22 18:40:21 +0100

    Vypísanie čísiel

--- main.js
+++ main.js
@@ -6,3 +6,10 @@
 // Z každého vylosovaného čísla vygeneruj následující HTML kód:
 // <span class="cislo">8</span>
 // ... který pak přidej dovnitř prvku <div id="vyherni-cisla">:
+
+let osudi = [];
+
+for (let i = 1; i <= 48; i = i + 1) {
+  osudi.push(i);
+}
+
